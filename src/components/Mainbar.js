0d9587b34fbecdea6cb55c6dc780fba6ce56d953
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-05-20 15:44:25 -0400

    commit

--- src/components/Mainbar.js
+++ src/components/Mainbar.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { links } from '../utils/data'
+import styled from 'styled-components'
 
 const Mainbar = () => {
-  return <div>Mainbar</div>
+  return (
+    <Wrapper>
+      {links.map((item) => {
+        const { id, path, text } = item
+        console.log(item)
+        return (
+          <NavLink key={id} to={path}>
+            {text}
+          </NavLink>
+        )
+      })}
+    </Wrapper>
+  )
 }
+const Wrapper = styled.div`
+  @media (max-width: 992px) {
+    display: none;
+  }
+  a {
+    margin-left: 1rem;
+  }
+`
 
 export default Mainbar
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { links } from '../utils/data'
+import styled from 'styled-components'
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <Wrapper>
+      {links.map((item) => {
+        const { id, path, text } = item
+        console.log(item)
+        return (
+          <NavLink key={id} to={path}>
+            {text}
+          </NavLink>
+        )
+      })}
+    </Wrapper>
+  )
 }
+const Wrapper = styled.div`
+  @media (min-width: 992px) {
+    display: none;
+  }
+  a {
+    margin-left: 1rem;
+  }
+`
 
 export default Sidebar
